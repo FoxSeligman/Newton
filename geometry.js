4376d8ff7eef9554db8276c8772f467a979dd7a1
MechaFox <aeseligman@gmail.com>
2015-04-29 18:31:15 -0700

    Stub in texturing

--- geometry.js
+++ geometry.js
@@ -13,30 +13,88 @@ function rect(va, vb, vc, vd) {
 }
 
 function rprism(sx, sy, sz) {
-    var va = [-sx/2, -sy/2,  sz/2];
-    var vb = [ sx/2, -sy/2,  sz/2];
-    var vc = [-sx/2,  sy/2,  sz/2];
-    var vd = [ sx/2,  sy/2,  sz/2];
+    var vertexArr = [
+        // Front face
+        -sx/2, -sy/2,  sz/2,
+         sx/2, -sy/2,  sz/2,
+         sx/2,  sy/2,  sz/2,
+        -sx/2,  sy/2,  sz/2,
 
-    var ve = [-sx/2, -sy/2, -sz/2];
-    var vf = [ sx/2, -sy/2, -sz/2];
-    var vg = [-sx/2,  sy/2, -sz/2];
-    var vh = [ sx/2,  sy/2, -sz/2];
+        // Back face
+        -sx/2, -sy/2, -sz/2,
+        -sx/2,  sy/2, -sz/2,
+         sx/2,  sy/2, -sz/2,
+         sx/2, -sy/2, -sz/2,
 
-    var vertexArr = [].concat(va, vb, vc, vd, ve, vf, vg, vh);
+        // Top face
+        -sx/2,  sy/2, -sz/2,
+        -sx/2,  sy/2,  sz/2,
+         sx/2,  sy/2,  sz/2,
+         sx/2,  sy/2, -sz/2,
+
+        // Bottom face
+        -sx/2, -sy/2, -sz/2,
+         sx/2, -sy/2, -sz/2,
+         sx/2, -sy/2,  sz/2,
+        -sx/2, -sy/2,  sz/2,
+
+        // Right face
+         sx/2, -sy/2, -sz/2,
+         sx/2,  sy/2, -sz/2,
+         sx/2,  sy/2,  sz/2,
+         sx/2, -sy/2,  sz/2,
+
+        // Left face
+        -sx/2, -sy/2, -sz/2,
+        -sx/2, -sy/2,  sz/2,
+        -sx/2,  sy/2,  sz/2,
+        -sx/2,  sy/2, -sz/2
+    ];
     var indexArr = [
-        0, 1, 2,    1, 2, 3,
-        1, 5, 3,    5, 3, 7,
-        5, 4, 7,    4, 7, 6,
-        4, 0, 6,    0, 6, 2,
-        4, 5, 0,    5, 0, 1,
-        2, 3, 6,    3, 6, 7
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23    // left
+    ];
+    var textureCoordinates = [
+        // Front
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Back
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Top
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Bottom
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Right
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Left
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0
     ];
 
-    return processArray(vertexArr, indexArr);
+    return processArray(vertexArr, indexArr, textureCoordinates);
 }
 
-function processArray(vertexArr, indexArr) {
+function processArray(vertexArr, indexArr, textureCoordinates) {
     var object = Object();
 
 
@@ -50,6 +108,13 @@ function processArray(vertexArr, indexArr) {
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArr), gl.STATIC_DRAW);
+
+
+    if (textureCoordinates) {
+        var textureBuffer = object.textureBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+    }
 
 
     object.itemSize = 3;
--- only.js
+++ only.js
@@ -7,53 +7,75 @@ window.onload = function init() {
     var canvas = document.getElementById( "gl-canvas" );
 
     gl = canvas.getContext("experimental-webgl");
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-
-    gl.viewportWidth = canvas.width;
-    gl.viewportHeight = canvas.height;
-    gl.enable(gl.DEPTH_TEST);
-    gl.clearColor( 0.5, 0.0, 0.0, 1.0 );
-
-
-    //Attach shaders
-    pr = gl.createProgram();
-
-    var vertexShader = getShader(gl, "vertex-shader");
-    var fragmentShader = getShader(gl, "fragment-shader");
-
-    gl.attachShader(pr, vertexShader);
-    gl.attachShader(pr, fragmentShader);
-
-    gl.linkProgram(pr);
-    if (!gl.getProgramParameter(pr, gl.LINK_STATUS))
-        alert("Could not initialise shaders");
+    if (gl) {
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+        gl.enable(gl.DEPTH_TEST);
+        gl.clearColor(0.5, 0.0, 0.0, 1.0);
+        //gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+        //gl.enable(gl.BLEND);
 
 
-    //Register program
-    gl.useProgram(pr);
+        //Attach shaders
+        pr = gl.createProgram();
+
+        var vertexShader = getShader(gl, "vertex-shader");
+        var fragmentShader = getShader(gl, "fragment-shader");
+
+        gl.attachShader(pr, vertexShader);
+        gl.attachShader(pr, fragmentShader);
+
+        gl.linkProgram(pr);
+        if (!gl.getProgramParameter(pr, gl.LINK_STATUS))
+            alert("Could not initialise shaders");
 
 
-    //Get locations
-    pr.vPositionLoc = gl.getAttribLocation(pr, "vPosition");
-    gl.enableVertexAttribArray(pr.vPositionLoc);
-
-    pr.pLoc = gl.getUniformLocation(pr, "P");
-    pr.mvLoc = gl.getUniformLocation(pr, "MV");
+        //Register program
+        gl.useProgram(pr);
 
 
-    //Generate uniform matrices
-    pr.P = mat4.create();
-    pr.MV = mat4.create();
+        //Get locations
+        pr.vPositionLoc = gl.getAttribLocation(pr, "vPosition");
+        gl.enableVertexAttribArray(pr.vPositionLoc);
+
+        //pr.aTextureCoordLoc = gl.getAttribLocation(pr, "aTextureCoord");
+        //gl.enableVertexAttribArray(pr.aTextureCoordLoc);
+
+        pr.pLoc = gl.getUniformLocation(pr, "P");
+        pr.mvLoc = gl.getUniformLocation(pr, "MV");
 
 
-    //Custom geometry
-    tri = rprism(5, 5, 1);
-    tri2 = rprism(1, 1, 1);
+        //Generate uniform matrices
+        pr.P = mat4.create();
+        pr.MV = mat4.create();
 
 
-    //Render
-    animate();
+        //Custom geometry
+        tri = rprism(5, 5, 0.5);
+        tri2 = rprism(1, 1, 1);
+
+
+        //Initialize textures
+        cubeTexture = gl.createTexture();
+        cubeImage = new Image();
+        cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); }
+        cubeImage.src = "cubetexture2.png";
+
+
+        //Render
+        animate();
+    }
 };
+
+function handleTextureLoaded(image, texture) {
+    console.log("handleTextureLoaded, image = " + image);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
 
 var time = 0;
 function animate() {
@@ -69,9 +91,11 @@ function animate() {
     mat4.identity(MV);
     mat4.translate(MV, MV, vec3.fromValues(0, 0, -6));
     mat4.rotateY(MV, MV, time);
+    //mat4.rotateZ(MV, MV, time);
     gl.uniformMatrix4fv(pr.pLoc, false, P);
     gl.uniformMatrix4fv(pr.mvLoc, false, MV);
 
+    renderObject(tri);
     renderObject(tri2);
 
     requestAnimationFrame(animate);
@@ -79,9 +103,20 @@ function animate() {
 
 function renderObject(object) {
     gl.bindBuffer(gl.ARRAY_BUFFER, object.vertexBuffer);
+    gl.vertexAttribPointer(pr.vPositionLoc, object.itemSize, gl.FLOAT, false, 0, 0);
+
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, object.indexBuffer);
 
-    gl.vertexAttribPointer(pr.vPositionLoc, object.itemSize, gl.FLOAT, false, 0, 0);
+    //if (object.textureBuffer)
+    //{
+    //    gl.bindBuffer(gl.ARRAY_BUFFER, object.textureBuffer);
+    //    gl.vertexAttribPointer(pr.aTextureCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    //
+    //    gl.activeTexture(gl.TEXTURE0);
+    //    gl.bindTexture(gl.TEXTURE_2D, cubeTexture);
+    //    gl.uniform1i(gl.getUniformLocation(pr, "uSampler"), 0);
+    //}
+
     gl.drawElements(gl.TRIANGLES, object.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
